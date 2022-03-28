191162bee48743b383231b131c07c324e51364dd
muhamadRamadhaniZ <danii251018@gmail.com>
2022-03-28 19:37:20 +0700

    memberikan dan mengakses modifier pd member class

---
+++ Pratikum empat/src/P4_TestEvent.java
@@ -0,0 +1,13 @@
+public class P4_TestEvent {
+    public static void main(String[] args) {
+        P4_Event even = new P4_Event("Yuk Ngaji", 200);
+        even.tglKegiatan = 10;
+        even.blnKegiatan = 11;
+        even.tahunKegiatan = 2018;
+        even.infoEvent();
+        System.out.println("=========================");
+        System.out.println("Tanggal Kegiatan : " + even.tglKegiatan + "/" + even.blnKegiatan + "/" + even.tahunKegiatan);
+        even.jmlPeserta = 100;
+        System.out.println("Jumlah Peserta : " +even.jmlPeserta);
+    }
+}
