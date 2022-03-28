3abb606721cf3b734a069ab92ff529b2585674d5
muhamadRamadhaniZ <danii251018@gmail.com>
2022-03-28 19:20:01 +0700

    mengakses class dan objek debgan dua file kode sumber yang berbeda

---
+++ Pratikum empat/src/P4_Orang1.java
@@ -0,0 +1,25 @@
+public class P4_Orang1 {
+    String nama;
+    int umur;
+    String warnaRambut;
+    String warnaKulit;
+    int tinggiBadan;
+    int beratBadan;
+
+    public P4_Orang1() {
+
+    }
+
+    public P4_Orang1(String nama) {
+        this.nama = nama;
+    }
+
+    public void menangis() {
+        System.out.println("hu....Hu...");
+    }
+
+    public void tertawa() {
+        System.out.println("Ha...haa..");
+    }
+}
+
